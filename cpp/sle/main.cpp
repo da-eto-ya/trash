d3a14795de3240ea56a05ee8ef8bd02c61d4c874
Ilya Pleshakov <da-eto@ya.ru>
2015-09-24 02:55:19 +0300

    sle: gauss method, backward direction

--- cpp/sle/main.cpp
+++ cpp/sle/main.cpp
@@ -45,60 +45,74 @@ long long gcd(long long a, long long b) {
 }
 
 template<class T>
-void printMatrix(matrix<T> &a) {
-    for (int i = 0; i < a.getRows(); ++i) {
-        for (int j = 0; j < a.getCols(); ++j) {
-            std::cout << a[i][j] << " ";
+bool upperInconsistent(matrix<T> &a) {
+    bool solvable = true;
+
+    for (int i = 0; i < a.getRows() && solvable; ++i) {
+        bool zeroed = true;
+
+        for (int j = 0; j < a.getCols() - 1 && zeroed; ++j) {
+            zeroed = (a[i][j] == 0);
         }
 
-        std::cout << std::endl;
+        if (zeroed) {
+            solvable = (a[i][a.getCols() - 1] == 0);
+        }
     }
+
+    return !solvable;
 }
 
-int main() {
-    // in
-    size_t n, m;
-    std::cin >> n >> m;
-    matrix<long long> &a = *new matrix<long long>(n, m + 1);
+template<class T>
+bool upperOneSolution(matrix<T> &a) {
+    size_t n = a.getCols() - 1;
 
-    for (size_t cuuudd = 0; cuuudd < n; ++cuuudd) {
-        for (size_t j = 0; j <= m; ++j) {
-            std::cin >> a[cuuudd][j];
+    if (a.getRows() < n) {
+        return false;
+    }
+
+    bool solvable = true;
+
+    for (size_t i = 0; i < n && solvable; ++i) {
+        solvable = (a[i][i] != 0);
+    }
+
+    for (size_t i = n; i < a.getRows() && solvable; ++i) {
+        for (size_t j = 0; j < a.getCols() && solvable; ++j) {
+            solvable = (a[i][j] == 0);
         }
     }
 
-    // solve
+    return solvable;
+}
 
-    // forward direction (upper triangle)
-    // fill zeros on column c (don't touch rows below r)
-    size_t col = 0, row = 0;
+template<class T>
+void gaussify(matrix<T> &a) {
+    size_t n = a.getRows();
+    size_t m = a.getCols() - 1;
+    size_t row = 0;
+    size_t col = 0;
 
     for (col = 0; col < m && row < n; ++col) {
-        std::cout << "\tCol: " << col << std::endl;
-
         // find row with non-zero in column c
         size_t cur = 0;
         for (cur = row; cur < n && a[cur][col] == 0; ++cur) { }
 
         if (cur < n && a[cur][col] != 0) {
-            std::cout << "found: " << cur << std::endl;
-
             if (cur != row) {
                 std::swap(a[cur], a[row]);
             }
 
-            printMatrix(a);
-
             // subtract a[r] from each next row
             for (size_t i = row + 1; i < n; ++i) {
-                std::cout << "row: " << i << std::endl;
-
                 if (a[i][col] != 0) {
                     long long g = gcd(a[row][col], a[i][col]);
                     long long dRow = a[row][col] / g;
                     long long dI = a[i][col] / g;
 
-                    for (size_t j = col; j <= m; ++j) {
+                    a[i][col] = 0;
+
+                    for (size_t j = col + 1; j <= m; ++j) {
                         a[i][j] = a[i][j] * dRow - a[row][j] * dI;
                     }
 
@@ -115,20 +129,64 @@ int main() {
                         }
                     }
                 }
-
-                printMatrix(a);
             }
 
             ++row;
         }
     }
+}
 
-    // check INF or NO
+template<class T>
+void diagonalize(matrix<T> &a) {
+    size_t n = a.getCols() - 1;
 
+    if (a.getRows() < n) {
+        return;
+    }
 
-    // out
-    std::cout << "Gauss:" << std::endl;
-    printMatrix(a);
+    for (size_t row = n - 1; row > 0; --row) {
+        for (size_t i = 0; i < row; ++i) {
+            long long g = gcd(a[row][row], a[i][row]);
+            long long dRow = a[row][row] / g;
+            long long dI = a[i][row] / g;
+
+            for (size_t j = i; j < a.getCols(); ++j) {
+                a[i][j] = a[i][j] * dRow - a[row][j] * dI;
+            }
+        }
+    }
+}
+
+int main() {
+    // in
+    size_t n, m;
+    std::cin >> n >> m;
+    matrix<long long> &a = *new matrix<long long>(n, m + 1);
+
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j <= m; ++j) {
+            std::cin >> a[i][j];
+        }
+    }
+
+    // solve
+    gaussify(a);
+
+    if (upperInconsistent(a)) {
+        std::cout << "NO" << std::endl;
+    } else if (upperOneSolution(a)) {
+        diagonalize(a);
+        std::cout << "YES" << std::endl;
+        std::cout.precision(15);
+
+        for (size_t i = 0; i < m; ++i) {
+            std::cout << std::fixed << (a[i][m] * 1.0) / a[i][i] << " ";
+        }
+
+        std::cout << std::endl;
+    } else {
+        std::cout << "INF" << std::endl;
+    }
 
     delete &a;
 
