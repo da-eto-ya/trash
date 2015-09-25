94bc344e10c370c1d1106d9396bba40c6d31d24b
Ilya Pleshakov <da-eto@ya.ru>
2015-09-26 00:15:39 +0300

    [trash] refs/new/constructor/destructor

--- cpp/simple-examples/newref.cpp
+++ cpp/simple-examples/newref.cpp
@@ -2,17 +2,34 @@
 
 class Hi {
 public:
-    Hi() {
-        std::cout << "Hi constructed" << std::endl;
+    int value;
+
+    Hi(int value) : value(value) {
+        std::cout << "Hi(" << this->value << ") constructed" << std::endl;
+    }
+
+    Hi(const Hi &other) : value(other.value) {
+        std::cout << "Hi(" << this->value << ") constructed from Hi(" << other.value << ")" << std::endl;
     }
 
     ~Hi() {
-        std::cout << "Hi destructed" << std::endl;
+        std::cout << "Hi(" << value << ") destructed" << std::endl;
+    }
+
+    Hi &operator=(const Hi &rhs) {
+        std::cout << "Hi(" << this->value << ") := Hi(" << rhs.value << ")" << std::endl;
+
+        if (this != &rhs) {
+            value = rhs.value;
+        }
+
+        return *this;
     }
 };
 
 int main() {
-    Hi *my = new Hi();
+    Hi &a0 = *new Hi(0);
+    Hi *a1 = new Hi(1);
 
     return 0;
 }
