9503c72327f3bd92b56fae17d8904756168aeac7
Ilya Pleshakov <da-eto@ya.ru>
2015-10-02 22:09:51 +0300

    [hs] simple functions

---
+++ haskell/simplest/some.hs
@@ -0,0 +1,21 @@
+import Data.Char
+
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int x y = if isDigit x && isDigit y then digitToInt x * 10 + digitToInt y else 100
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist x y = sqrt $ (fst x - fst y) ^ 2 + (snd x - snd y) ^ 2
+
+doubleFact :: Integer -> Integer
+doubleFact 1 = 1
+doubleFact 2 = 2
+doubleFact n = n * doubleFact (n - 2)
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
+            | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
+
+
+main :: IO ()
+main = putStrLn "hello"
