b075016114bc2fac90b5f0b65a625b5492d79839
Ilya Pleshakov <da-eto@ya.ru>
2015-10-03 01:53:52 +0300

    [hs] fib linear

--- haskell/simplest/some.hs
+++ haskell/simplest/some.hs
@@ -11,11 +11,13 @@ doubleFact 1 = 1
 doubleFact 2 = 2
 doubleFact n = n * doubleFact (n - 2)
 
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
-            | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
-
+fibonacci :: Integer -> Integer
+fibonacci = fibonacci' 0 1 where
+  fibonacci' :: Integer -> Integer -> Integer -> Integer
+  fibonacci' f1 _  0 = f1
+  fibonacci' _  f2 1 = f2
+  fibonacci' f1 f2 n | n > 0 = fibonacci' f2 (f1 + f2) (n - 1)
+                     | n < 0 = fibonacci' (f2 - f1) f1 (n + 1)
 
 main :: IO ()
 main = putStrLn "hello"
