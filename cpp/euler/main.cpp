32b3d6a208240961acf07b1a1ad56b91868be92f
Ilya Pleshakov <da-eto@ya.ru>
2015-10-12 23:57:15 +0300

    [wip] euler algo

---
+++ cpp/euler/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    size_t v, e;
+    std::cin >> v >> e;
+
+    for (int i = 0; i < e; ++i) {
+        // TODO: input edges
+    }
+
+    // TODO: solve
+
+    std::cout << "NONE" << std::endl;
+
+    return 0;
+}
