6ecb0d082d72c1a23c038461e43bfbaeab9e9116
Ilya Pleshakov <da-eto@ya.ru>
2015-10-13 17:56:37 +0300

    [wip] euler algo

--- cpp/euler/main.cpp
+++ cpp/euler/main.cpp
@@ -1,16 +1,86 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main() {
-    size_t v, e;
-    std::cin >> v >> e;
+std::vector<std::vector<size_t> > g;
+size_t v, e;
+std::vector<size_t> path;
 
-    for (int i = 0; i < e; ++i) {
-        // TODO: input edges
+bool all_vertices_even() {
+    for (size_t i = 0; i < v; ++i) {
+        int sum = 0;
+
+        for (size_t j = 0; j < v; ++j) {
+            sum += g[i][j];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
     }
 
-    // TODO: solve
+    return true;
+}
 
-    std::cout << "NONE" << std::endl;
+void find_euler_path(size_t i) {
+    for (size_t j = 0; j < v; ++j) {
+        if (g[i][j] > 0) {
+            --g[i][j], --g[j][i];
+
+            find_euler_path(j);
+        }
+    }
+
+    path.push_back(i);
+}
+
+bool check_all_edges() {
+    for (size_t i = 0; i < v; ++i) {
+        int sum = 0;
+
+        for (size_t j = 0; j < v; ++j) {
+            sum += g[i][j];
+        }
+
+        if (sum != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    std::cin >> v >> e;
+    g.resize(v);
+
+    for (size_t i = 0; i < v; ++i) {
+        g[i].resize(v, 0);
+    }
+
+    for (size_t i = 0; i < e; ++i) {
+        size_t a, b;
+        std::cin >> a >> b;
+        --a, --b;
+        ++g[a][b], ++g[b][a];
+    }
+
+    bool result = false;
+
+    if (all_vertices_even()) {
+        find_euler_path(0);
+        result = check_all_edges();
+    }
+
+    if (result) {
+        for (size_t i = 0; i < path.size() - 1; ++i) {
+            std::cout << path[i] + 1 << " ";
+        }
+
+        std::cout << std::endl;
+    } else {
+        std::cout << "NONE" << std::endl;
+    }
 
     return 0;
 }
