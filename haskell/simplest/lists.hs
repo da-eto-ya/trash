30e911b2dde1f078c7cd2ac874a38ee1a06c16e8
Ilya Pleshakov <da-eto@ya.ru>
2015-10-26 18:02:55 +0300

    [haskell] lists (simple concepts)

--- haskell/simplest/lists.hs
+++ haskell/simplest/lists.hs
@@ -6,3 +6,8 @@ addTwoElements x y = (x :) . (y :)
 
 nTimes:: a -> Int -> [a]
 nTimes x c = take c xs where xs = x : xs
+
+oddsOnly :: Integral a => [a] -> [a]
+oddsOnly []       = []
+oddsOnly (x : xs) = if odd x then x : oddsOnly xs else oddsOnly xs
+
