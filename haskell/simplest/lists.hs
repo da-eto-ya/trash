0b44f8d813f162d556c5dafc4af6f615774585fc
Ilya Pleshakov <da-eto@ya.ru>
2015-10-26 18:29:27 +0300

    [haskell] lists

--- haskell/simplest/lists.hs
+++ haskell/simplest/lists.hs
@@ -11,3 +11,9 @@ oddsOnly :: Integral a => [a] -> [a]
 oddsOnly []       = []
 oddsOnly (x : xs) = if odd x then x : oddsOnly xs else oddsOnly xs
 
+sum3 :: Num a => [a] -> [a] -> [a] -> [a]
+sum3 xs ys zs = sum2 (sum2 xs ys) zs where
+  sum2 (x:xs) (y:ys) = (x + y) : sum2 xs ys
+  sum2 []     ys     = ys
+  sum2 xs     []     = xs
+
