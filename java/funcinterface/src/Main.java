9cc6439f2fa7d35f1b02d26fddc5f621257e09ec
Ilya Pleshakov <da-eto@ya.ru>
2015-11-02 23:09:39 +0300

    [java] functional interfaces example

---
+++ java/funcinterface/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+
+        return (x -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x));
+    }
+
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+        System.out.println(safeStringLength.apply("1234"));
+        System.out.println(safeStringLength.apply(""));
+        System.out.println(safeStringLength.apply(null));
+    }
+}
