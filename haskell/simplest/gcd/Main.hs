00cf5a2cc07e20fa461e1a6aa395120d73da98d2
Ilya Pleshakov <da-eto@ya.ru>
2015-11-07 12:30:19 +0300

    [haskell] simplify gcd

--- haskell/simplest/gcd/Main.hs
+++ haskell/simplest/gcd/Main.hs
@@ -5,11 +5,10 @@ import Prelude hiding (gcd)
 gcd :: [Int] -> Int
 gcd []           = 0
 gcd [x]          = x
-gcd (x : y : xs) = gcd $ gcd' x y : xs where
+gcd (x : y : xs) = gcd $ gcd' (abs x) (abs y) : xs where
   gcd' 0 b = b
   gcd' a 0 = a
-  gcd' a b  | a >= b    = gcd' (a `mod` b) b
-            | otherwise = gcd' a (b `mod` a)
+  gcd' a b = gcd' b (a `rem` b)
 
 main :: IO ()
 main = interact (show . gcd . map read . words)
