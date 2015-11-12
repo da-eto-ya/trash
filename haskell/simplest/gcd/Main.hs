9ae126145d5c88fbc779521d50af53cc8032f697
Ilya Pleshakov <da-eto@ya.ru>
2015-11-12 23:58:55 +0300

    [haskell] try style

--- haskell/simplest/gcd/Main.hs
+++ haskell/simplest/gcd/Main.hs
@@ -3,9 +3,9 @@ module Main (main) where
 import Prelude hiding (gcd)
 
 gcd :: [Int] -> Int
-gcd []           = 0
-gcd [x]          = x
-gcd (x : y : xs) = gcd $ gcd' (abs x) (abs y) : xs where
+gcd []       = 0
+gcd [x]      = x
+gcd (x:y:xs) = gcd $ gcd' (abs x) (abs y) : xs where
   gcd' 0 b = b
   gcd' a 0 = a
   gcd' a b = gcd' b (a `rem` b)
