a92aa70b49864d72b944dfab4d00872499bdb283
Ilya Pleshakov <da-eto@ya.ru>
2017-04-17 03:11:09 +0300

    [js] bloom filter simple demo

--- js/bloom-filter/BloomFilter.js
+++ js/bloom-filter/BloomFilter.js
@@ -1,36 +1,60 @@
 (function (exports) {
     exports.BloomFilter = BloomFilter;
 
-    function BloomFilter(storageSize, hashCount, seed) {
-        this.storageSize = storageSize;
-        this.storage = new Uint32Array(Math.ceil(storageSize / 32));
+    /**
+     * Bloom filter constructor
+     *
+     * @param {int} size filter size in bits
+     * @param {int} hashCount count of hash functions
+     * @param {int} [seed] optionally passed seed for hashing
+     * @constructor
+     */
+    function BloomFilter(size, hashCount, seed) {
+        this.size = size;
         this.hashCount = hashCount;
-        this.seed = (seed === undefined) ? 0x1c981dc5 : seed;
+        this.seed = (seed === undefined) ? 0x1c981dc7 : seed;
+        this.storage = new Uint32Array(Math.ceil(size / 32));
         this._hashResult = new Uint32Array(hashCount);
     }
 
-    BloomFilter.prototype.hashes = function (v) {
+    /**
+     * Generate hashCount hash values for given key
+     *
+     * @param {string|*} key value for hashing
+     * @returns {Uint32Array} array of hashCount values in [0 ... size-1] range
+     */
+    BloomFilter.prototype.hash = function (key) {
         var result = this._hashResult,
-            m = this.storageSize;
-        var a = fnv(v + "", this.seed) % m,
-            b = fnv(v + "") % m;
+            m = this.size;
+        var a = fnv(key + "") % m,
+            b = fnv(key + "", this.seed);
 
         for (var i = 0, c = this.hashCount; i < c; ++i) {
-            result[i] = a >= 0 ? a : a + m;
-            a = (a + b) % m;
+            result[i] = ((a + b * i) >>> 0) % m;
         }
 
         return result;
     };
 
-    BloomFilter.prototype.add = function (v) {
-        this.hashes(v).forEach(function (h) {
+    /**
+     * Adds given key to set
+     *
+     * @param {string|*} key
+     */
+    BloomFilter.prototype.add = function (key) {
+        this.hash(key).forEach(function (h) {
             this.storage[Math.floor(h / 32)] |= 1 << (h % 32);
         }, this);
     };
 
-    BloomFilter.prototype.test = function (v) {
-        return this.hashes(v).every(function (h) {
+    /**
+     * Tests that given key is in set
+     *
+     * @param {string|*} key
+     * @returns {boolean} false <=> key is not in set, true <=> key probably is in set
+     */
+    BloomFilter.prototype.test = function (key) {
+        return this.hash(key).every(function (h) {
             return (this.storage[Math.floor(h / 32)] & (1 << (h % 32))) !== 0;
         }, this);
     };
@@ -41,7 +65,7 @@
      *
      * @param {string} str the input value
      * @param {int} [seed] optionally pass the hash seed
-     * @return {int}
+     * @returns {int}
      */
     function fnv(str, seed) {
         var i, l;
